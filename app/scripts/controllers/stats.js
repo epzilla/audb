c5711456d41b7ee2fdb5626e6707e246a3077c05
Adam Epling <adamepling@gmail.com>
2014-01-21 21:39:56 -0600

    Aw yiss

--- app/scripts/controllers/stats.js
+++ app/scripts/controllers/stats.js
@@ -1,12 +1,20 @@
 'use strict';
 
 angular.module('audbApp')
-  .controller('StatsCtrl', function ($scope, $http) {
+  .controller('StatsCtrl', function ($scope, $http, $window, Auth) {
     $scope.years = [];
     $scope.reverseYears = [];
     $scope.selectedTeams = [];
     $scope.endYear = new Date().getFullYear();
     $scope.startYear = 1892;
+    $scope.user = {};
+    $scope.record = {};
+    var breakpoint = 768;
+
+    Auth.currentUser().$promise.then( function(user) {
+      $scope.user = user;
+    });
+
     for (var i = $scope.startYear; i <= $scope.endYear; i++) {
       $scope.years.push(i);
     }
@@ -44,9 +52,38 @@ angular.module('audbApp')
         confs: confs
       }).success( function (data) {
         $scope.games = data;
-        console.log(data);
+        $scope.record = {
+          w: 0,
+          l: 0,
+          t: 0,
+          secW: 0,
+          secL: 0,
+          secT: 0
+        };
+        for (var i = 0; i < data.length; i++) {
+          switch(data[i].Result) {
+            case 'W':
+              $scope.record.w++;
+              if (data[i].SEC === 'y') {
+                $scope.record.secW++;
+              }
+              break;
+            case 'L':
+              $scope.record.l++;
+              if (data[i].SEC === 'y') {
+                $scope.record.secL++;
+              }
+              break;
+            default:
+              $scope.record.t++;
+              if (data[i].SEC === 'y') {
+                $scope.record.secT++;
+              }
+          }
+        }
         angular.element('#reset').show();
         angular.element('#stat-form').slideUp(650);
+
       });
     };
 
@@ -57,4 +94,37 @@ angular.module('audbApp')
       $scope.games = [];
       $scope.selectedTeams = [];
     };
+
+    $scope.toggleAttended = function(gameID) {
+      var game = angular.element('#' + gameID);
+      if (game.hasClass('yes')) {
+        game.removeClass('yes').html('<span class="glyphicon glyphicon-minus"></span>');
+      } else {
+        game.addClass('yes').html('<span class="glyphicon glyphicon-ok"></span>');
+      }
+      $http.post('/api/updateAttendance/' + gameID).success( function(user) {
+        $scope.user = user;
+      });
+    };
+
+    $scope.smallScreenAttend = function(gameID) {
+      if ($scope.isSmallScreen) {
+        angular.element('#game-row-'+gameID).toggleClass('attended');
+        $http.post('/api/updateAttendance/' + gameID).success( function(user) {
+          $scope.user = user;
+        });
+      }
+    };
+
+    $scope.didAttend = function(gameID) {
+      var games = $scope.user.games;
+      for (var i = 0; i < games.length; i++) {
+        if (games[i] === gameID) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    $scope.isSmallScreen = $window.innerWidth < breakpoint ? true : false;
   });
