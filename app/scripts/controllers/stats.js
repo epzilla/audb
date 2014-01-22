e868ac9ac67c6f02ed78b839d1d8ab3b87a80eda
Adam Epling <adamepling@gmail.com>
2014-01-21 21:28:57 -0600

    Good progress

--- app/scripts/controllers/stats.js
+++ app/scripts/controllers/stats.js
@@ -4,6 +4,7 @@ angular.module('audbApp')
   .controller('StatsCtrl', function ($scope, $http) {
     $scope.years = [];
     $scope.reverseYears = [];
+    $scope.selectedTeams = [];
     $scope.endYear = new Date().getFullYear();
     $scope.startYear = 1892;
     for (var i = $scope.startYear; i <= $scope.endYear; i++) {
@@ -12,8 +13,48 @@ angular.module('audbApp')
     for (i = $scope.endYear; i >= $scope.startYear; i--) {
       $scope.reverseYears.push(i);
     }
-    $http.get('/api/conferences').success(function(confs) {
+
+    $http.get('/api/conferences').success( function (confs) {
       $scope.conferences = confs;
-      console.log(confs);
     });
+
+    $scope.submitForm = function () {
+      angular.element('#stat-form button').prop('disabled', true);
+      var confs = [];
+      var teams = [];
+      var x;
+      for (var i = 0; i < $scope.selectedTeams.length; i++) {
+        if ($scope.selectedTeams[i] === 'ALL-OPP') {
+          teams.push('ALL-OPP');
+          confs = [];
+          break;
+        } else {
+          x = $scope.selectedTeams[i].indexOf('Conf: ');
+          if (x !== -1) {
+            confs.push($scope.selectedTeams[i].replace('Conf: ', ''));
+          } else {
+            teams.push($scope.selectedTeams[i]);
+          }
+        }
+      }
+      $http.post('/api/statsByOpponent', {
+        startYear: $scope.startYear,
+        endYear: $scope.endYear,
+        teams: teams,
+        confs: confs
+      }).success( function (data) {
+        $scope.games = data;
+        console.log(data);
+        angular.element('#reset').show();
+        angular.element('#stat-form').slideUp(650);
+      });
+    };
+
+    $scope.reset = function () {
+      angular.element('#stat-form button').prop('disabled', false);
+      angular.element('#reset').hide();
+      angular.element('#stat-form').slideDown(300);
+      $scope.games = [];
+      $scope.selectedTeams = [];
+    };
   });
