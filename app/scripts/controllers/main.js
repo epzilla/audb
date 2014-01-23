d8a6b877a6133322a1d8a81f5109be0dbe305b1f
Adam Epling <adamepling@gmail.com>
2014-01-23 14:18:51 -0600

    more local stuff. why not?!

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,7 @@
 
 angular.module('audbApp')
   .controller('MainCtrl', function ($scope, $http, Auth, localStorageService) {
+    var ls = localStorageService;
     if (angular.element('#nav-menu-collapse').hasClass('in')) {
       angular.element('.navbar-toggle').click();
     }
@@ -10,52 +11,67 @@ angular.module('audbApp')
     Auth.currentUser().$promise.then(function(user){
       if (user._id) {
         $scope.user = user;
-        $http.get('/api/gamesByUser/' + $scope.user._id).success(function(data) {
-          $scope.games = data;
-          $scope.record = {
-            w: 0,
-            l: 0,
-            t: 0,
-            secW: 0,
-            secL: 0,
-            secT: 0,
-            total: 0
-          };
-          for (var i = 0; i < data.length; i++) {
+        $scope.record = {
+          w: 0,
+          l: 0,
+          t: 0,
+          secW: 0,
+          secL: 0,
+          secT: 0,
+          total: 0
+        };
+        $scope.games = ls.get('myGames');
+        if ($scope.games) {
+          for (var i = 0; i < $scope.games.length; i++) {
             $scope.record.total++;
-            switch(data[i].Result) {
+            switch($scope.games[i].Result) {
               case 'W':
                 $scope.record.w++;
-                if (data[i].SEC === 'y') {
+                if ($scope.games[i].SEC === 'y') {
                   $scope.record.secW++;
                 }
                 break;
               case 'L':
                 $scope.record.l++;
-                if (data[i].SEC === 'y') {
+                if ($scope.games[i].SEC === 'y') {
                   $scope.record.secL++;
                 }
                 break;
               default:
                 $scope.record.t++;
-                if (data[i].SEC === 'y') {
+                if ($scope.games[i].SEC === 'y') {
                   $scope.record.secT++;
                 }
             }
           }
-        });
+        } else {
+          $http.get('/api/gamesByUser/' + $scope.user._id).success(function(data) {
+            $scope.games = data;
+            ls.add('myGames', data);
+            for (var i = 0; i < data.length; i++) {
+              $scope.record.total++;
+              switch(data[i].Result) {
+                case 'W':
+                  $scope.record.w++;
+                  if (data[i].SEC === 'y') {
+                    $scope.record.secW++;
+                  }
+                  break;
+                case 'L':
+                  $scope.record.l++;
+                  if (data[i].SEC === 'y') {
+                    $scope.record.secL++;
+                  }
+                  break;
+                default:
+                  $scope.record.t++;
+                  if (data[i].SEC === 'y') {
+                    $scope.record.secT++;
+                  }
+              }
+            }
+          });
+        }
       }
     });
-    $scope.localStuff = {};
-    $scope.testLocalStorage = function () {
-      var someJSON = {
-        hair: 'blonde',
-        age: 28,
-        city: 'Akron'
-      };
-      localStorageService.add('localStorageKey',someJSON);
-      $scope.localStuff = localStorageService.get('localStorageKey');
-      console.log($scope.localStuff);
-    };
-
   });
